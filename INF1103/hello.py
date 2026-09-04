292f335c97f3857be5dac06cd9ed9ae5abf986ce
Jun Long <2602060@sit.singaporetech.edu.sg>
2026-09-04 13:12:59 +0800

    Fun Conditions

--- INF1103/hello.py
+++ INF1103/hello.py
@@ -26,8 +26,19 @@ follwers -= 10
 print("Day 3:", follwers)
 
 # Activity 4
+# username = input("Enter Username: ")
+# age = input("Enter Age: ")
+# category = input("Enter Content Category: ")
+
+# print("\n Instagram Profile")
+# print("==========")
+# print("Username:", username)
+# print("Age:", age)
+# print("Content Category:", category)
+
+# Activity 5
 username = input("Enter Username: ")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\n Instagram Profile")
@@ -35,3 +46,5 @@ print("==========")
 print("Username:", username)
 print("Age:", age)
 print("Content Category:", category)
+if age>40 and category == "fun":
+    print("You are old, what is fun for you??")
