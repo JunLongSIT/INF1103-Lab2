08c4b0aeb7a110eb4cd26a64116bd5fde2863117
Jun Long <2602060@sit.singaporetech.edu.sg>
2026-09-04 13:06:52 +0800

    Assignment Variables

--- INF1103/hello.py
+++ INF1103/hello.py
@@ -4,6 +4,8 @@ print("Welcome here")
 print("My first post!")
 print("==========")
 
+# 'q' to escape the terminal after running
+
 # Activity 2
 username = "cool_creator"
 bio = "Fun Blogger"
@@ -12,4 +14,14 @@ follwers = 100
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", follwers)
+
+# Activity 3
+follwers += 50
+print("Day 1:", follwers)
+
+follwers += 20
+print("Day 2:", follwers)
+
+follwers -= 10
+print("Day 3:", follwers)
 
